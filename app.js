d36fc90cffb6cdd5d2ec079664c9f12e2eff08b9
Gayani Samaraweera <156066338+desundara@users.noreply.github.com>
2025-06-09 12:42:39 +0530

    new commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ console.log("Click!");
     let inputText = document.getElementById("txtInput").value;
 
     if(randomNumber==inputText){
-        document.getElementById("imgBox").innerHTML= '<img width="300" src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExb2k1dXR1dXllZXpkc2o4dnllc2s5emRyNmtyeDdvNjAwZmQ0MXYyeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/etKSrsbbKbqwW6vzOg/giphy.gif" alt="">'
+        document.getElementById("imgBox").innerHTML= '<img width="250" src="img/win.gif" alt="">'
         alert("WIN!!")
         h5Lbl.innerText="Congratulations! YOU WIN.!!!";
     }else if(randomNumber<inputText){
